3ca2a8beba91d4cfc54d98778034d52e59c0c649
Christian Crites <ccrites98@gmail.com>
2023-10-12 11:27:36 -0400

    Loopers

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+//Jamie, pull up that loop function
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 5) {
+            System.out.println("I love Java");
+            count++;
+
+
+        }
+    }
+}
